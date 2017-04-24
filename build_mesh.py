970220614b965a15201e0011f837184934beee3e
tjlaboss <travis@ktc-technology.com>
2017-04-24 16:58:55 -0400

    Structure for TREAT checkerboard

--- build_mesh.py
+++ build_mesh.py
@@ -13,7 +13,6 @@ from treat_mesh import Treat_Mesh
 EXPORT = True
 PLOT = True
 
-
 # Extract the geometry from an existing summary
 summ = openmc.Summary("summary.h5")
 geom = summ.geometry
@@ -30,14 +29,13 @@ mesh_lib.energy_groups = two_groups
 # and the transport cross sections
 mesh_lib.mgxs_types = ['fission', 'nu-fission', 'transport']
 mesh_lib.by_nuclide = True
-#mesh_lib.nuclides = ["U235", "U238"]
+# mesh_lib.nuclides = ["U235", "U238"]
 # TODO: select all the nuclides from `mats`
 mesh_lib.domain_type = "mesh"
 
-
 # Define a mesh
 # Instantiate a tally Mesh
-mesh = Treat_Mesh(mesh_id=1, geometry = geom)
+mesh = Treat_Mesh(mesh_id = 1, geometry = geom)
 # FIXME: For some reason, in tallies.xml, the mesh gets exported 3 times!!
 core_lat = geom.get_all_lattices()[100]
 xdist = -core_lat.lower_left[0]
@@ -51,12 +49,12 @@ mesh.upper_right[-1] = ztop
 mesh.type = 'regular'
 mesh.dimension = core_lat.shape
 
-mesh_lib.domains = [mesh,]
+mesh_lib.domains = [mesh]
 mesh_lib.build_library()
+mesh_lib.dump_to_file("treat_mesh_lib")
 
 # Instantiate tally Filter
 mesh_filter = openmc.MeshFilter(mesh)
-
 
 # Create the material lib
 material_lib = mgxs.Library(geom)
@@ -67,18 +65,20 @@ material_lib.domains = mats.values()
 material_lib.by_nuclide = True
 material_lib.build_library()
 
-# Instantiate the Tally
-tally = openmc.Tally(name='mesh tally')
-tally.filters = [mesh_filter]
-tally.scores = ["fission", "nu-fission"]
 
-# Create a "tallies.xml" file for the MGXS Library
-tallies_file = openmc.Tallies()
-
-
-mesh_lib.add_to_tallies_file(tallies_file, merge=True)
-material_lib.add_to_tallies_file(tallies_file, merge = True)
-
+def make_tallies():
+	# Instantiate the Tally
+	tally = openmc.Tally(name = 'mesh tally')
+	tally.filters = [mesh_filter]
+	tally.scores = ["fission", "nu-fission"]
+	
+	# Create a "tallies.xml" file for the MGXS Library
+	tallies_file = openmc.Tallies()
+	tallies_file.append(tally)
+	
+	mesh_lib.add_to_tallies_file(tallies_file, merge = True)
+	material_lib.add_to_tallies_file(tallies_file, merge = True)
+	return tallies_file
 
 
 def plot_mgxs(nuc, xs_lib, xs_df, g, groups, x0 = -xdist, x1 = xdist, n = 19):
@@ -97,24 +97,24 @@ def plot_mgxs(nuc, xs_lib, xs_df, g, groups, x0 = -xdist, x1 = xdist, n = 19):
 	
 	xlist = pylab.linspace(x1, x0, n)
 	xs_scale = "macro"
-	#nuc_xs = xs_lib.get_xs(order_groups = "decreasing", xs_type = xs_scale, groups = g).
+	# nuc_xs = xs_lib.get_xs(order_groups = "decreasing", xs_type = xs_scale, groups = g).
 	
 	group_df = xs_df[xs_df["group in"] == g]
 	y_df = group_df[group_df[('mesh 1', 'y')] == 9]
-	#y_at_z = y_df[y_df[("mesh 1", "z")] == 9]
+	# y_at_z = y_df[y_df[("mesh 1", "z")] == 9]
 	yvals = y_df['mean']
 	uncert = y_df['std. dev.']
 	
 	# FIXME: This returns an empty array
-	#nuc_df = xs_df[xs_df['nuclide'] == nuc]['mean']
-	#print(nuc_df
-	#nuc_matrix = nuc_df.as_matrix()
-	#print(nuc_matrix)
+	# nuc_df = xs_df[xs_df['nuclide'] == nuc]['mean']
+	# print(nuc_df
+	# nuc_matrix = nuc_df.as_matrix()
+	# print(nuc_matrix)
 	
 	# plotting stuff for later
 	ylist = yvals
 	pylab.plot(xlist, ylist, drawstyle = "steps", label = "$\Sigma$")
-	pylab.plot(xlist, ylist + uncert, "red", drawstyle = "steps", alpha = 0.5, label="+/- 1sigma")
+	pylab.plot(xlist, ylist + uncert, "red", drawstyle = "steps", alpha = 0.5, label = "+/- 1sigma")
 	pylab.plot(xlist, ylist - uncert, "red", drawstyle = "steps", alpha = 0.5)
 	pylab.legend(loc = "upper left")
 	pylab.grid()
@@ -122,46 +122,37 @@ def plot_mgxs(nuc, xs_lib, xs_df, g, groups, x0 = -xdist, x1 = xdist, n = 19):
 	title_string = "{0} {1}scopic Cross Section".format("Total", xs_scale.title())
 	pylab.xlabel("Radial distance (cm)")
 	pylab.ylabel("$\Sigma$ (cm$^{-1}$)")
-	pylab.title(title_string, {"fontsize":14})
+	pylab.title(title_string, {"fontsize": 14})
 	pylab.show()
-	
-
-
 
 
 if __name__ == "__main__":
 	# Add tally to collection
-	tallies_file.append(tally)
-	# Filter
-	mesh_filter = openmc.Filter(mesh)
-	
+	tallies_file = make_tallies()
 	if EXPORT:
 		tallies_file.export_to_xml("test_model/tallies.xml")
 	
+	# Filter
+	mesh_filter = openmc.Filter(mesh)
+	
 	# Examine the data after the run
-	#sp = openmc.StatePoint('test_model/statepoint.50.h5')
+	# sp = openmc.StatePoint('test_model/statepoint.50.h5')
 	sp = openmc.StatePoint('test_model/statepoint.30.h5')
 	
-	
-	# FIXME: This appears to be loading an openmc.Mesh, not a Treat_Mesh
 	mesh_lib.load_from_statepoint(sp)
 	mesh_lib.domains = [mesh]
 	for domain in mesh_lib.domains:
 		for mgxs_type in mesh_lib.mgxs_types:
 			xs = mesh_lib.get_mgxs(domain, mgxs_type)
 			xs.domain = mesh
-			
+	
 	material_lib.load_from_statepoint(sp)
 	
 	fission_mgxs = mesh_lib.get_mgxs(mesh, "transport")
 	fission_df = fission_mgxs.get_pandas_dataframe(nuclides = ["C0"])
-	#print(fission_df.head(17), fission_df.tail(17))
-	#fission_mgxs.print_xs()
+	# print(fission_df.head(17), fission_df.tail(17))
+	# fission_mgxs.print_xs()
 	
 	if PLOT:
 		# Plot stuff
 		plot_mgxs("U235", fission_mgxs, fission_df, 1, two_groups)
-	
-
-
-
---
+++ build_moc_checker.py
@@ -0,0 +1,105 @@
+# MOC Checkerboard
+#
+# Given the mesh tally results from OpenMC, take the cross sections
+# and plug them into an MOC model
+
+import openmoc
+import openmoc.materialize
+import openmc.openmoc_compatible
+import openmoc.plotter as plt
+import openmc.mgxs as mgxs
+import numpy as np
+from build_mesh import mesh, Treat_Mesh
+
+print(type(mesh))
+# Load the Monte Carlo results
+fname = "test_model/statepoint.30.h5"
+sp = openmc.StatePoint(fname)
+mesh_lib = mgxs.Library.load_from_file(filename = "treat_mesh_lib")
+mesh_lib.domains = [mesh]
+# FIXME: this next line breaks because it thinks it has a Mesh, not a Treat_Mesh
+#mesh_lib.load_from_statepoint(sp)
+x_width, y_width, z_width = (mesh.upper_right - mesh.lower_left)/mesh.dimension
+
+
+#######################################
+# Dataframes
+#######################################
+
+# FIXME: it doesn't like the "stride" attribute for "transport"...
+# FIXME: ...and it is only looking for Tally 10001, when it needs Tally 1.
+
+#transport = mesh_lib.get_mgxs(domain=mesh, mgxs_type="transport")
+#nu_fission = mesh_lib.get_mgxs(domain=mesh, mgxs_type="nu-fission")
+
+# Get the dataframes
+mgxs_dfs = {}
+#mgxs_dfs['transport'] = transport.get_pandas_dataframe(nuclides='sum')
+#mgxs_dfs['nu-fission'] = nu_fission.get_pandas_dataframe(nuclides='sum')
+
+#######################################
+# Geometry
+#######################################
+
+# Build a checkerboard geometry in OpenMOC
+lattice = openmoc.Lattice(name = 'TREAT lattice')
+lattice.setWidth(x_width, y_width)
+
+nx, ny = mesh.dimension[:2]
+universes = [[None for i in range(nx)] for j in range(ny)]
+
+for i in range(nx):
+	for j in range(ny):
+		
+		c = openmoc.Cell()
+		m = openmoc.Material()
+		m.setNumEnergyGroups(2)
+		
+		for key in mgxs_dfs:
+			df = mgxs_dfs[key]
+			x_df = df[df[('mesh 1', 'x')] == i + 1]
+			y_at_x = x_df[x_df[("mesh 1", "y")] == j + 1]
+			
+			'''
+			if key == "total":
+				m.setSigmaT(y_at_x['mean'].values)
+			elif key == "chi":
+				m.setChi(y_at_x['mean'].values)
+			elif key == "nu-fission":
+				m.setNuSigmaF(y_at_x['mean'].values)
+			elif key == "nu-scatter":
+				m.setSigmaS(y_at_x['mean'].values)
+			'''
+			if key == "nu-fission":
+				m.setNuSigmaF(y_at_x['mean'].values)
+		
+		c.setFill(m)
+		u = openmoc.Universe()
+		u.addCell(c)
+		universes[j][i] = u
+
+lattice.setUniverses([universes])
+
+root_universe = openmoc.Universe(name="root universe")
+root_cell = openmoc.Cell(name="root cell")
+root_cell.setFill(lattice)
+
+# Make some boundaries
+min_x = openmoc.XPlane(x=mesh.lower_left[0])
+max_x = openmoc.XPlane(x=mesh.upper_right[0])
+min_y = openmoc.YPlane(y=mesh.lower_left[1])
+max_y = openmoc.YPlane(y=mesh.upper_right[1])
+surfs = [min_x, max_x, min_y, max_y]
+for s in surfs:
+	s.setBoundaryType(openmoc.REFLECTIVE)
+root_cell.addSurface(+1, min_x)
+root_cell.addSurface(-1, max_x)
+root_cell.addSurface(+1, min_y)
+root_cell.addSurface(-1, max_y)
+
+root_universe.addCell(root_cell)
+geom = openmoc.Geometry()
+geom.setRootUniverse(root_universe)
+
+plt.plot_cells(geom)
+plt.plot_materials(geom)
